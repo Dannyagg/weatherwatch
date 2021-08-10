e8534d4fcc066b0288cb1c314ba69368161cb3e9
Daniel Agbenu <borgadee@gmail.com>
2021-08-10 10:08:47 -0400

    added weather app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import WeatherSearch from './components/WeatherSearch';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+console.log(process.env.React_API_KEY)
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1 className="heading"> Weather Watch </h1>
+
+      <WeatherSearch />
+      
+
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/WeatherSearch.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import TheWeather from './TheWeather';
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+function WeatherSearch() {
+
+    const [local, setLocal] = useState();
+    const [location, setLocation] = useState({
+        longitude: null,
+        latitude: null,
+        nation: null,
+        city: null
+    });
+
+    const handleChange = (event) => {
+        setLocal(event.target.value);
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+
+        const myApi = 'de07d1446efc27eb166e53b0fc109012'
+        const response = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${local},US&appid=${myApi}`)
+            .then(response => response.json())
+        const { lon, lat, name, country, zip } = response;
+        console.log(response);
+
+        setLocation({ longitude: lon, latitude: lat, city: name, nation: country, area: zip });
+
+    }
+
+    return (
+
+        <div>
+
+            {/* <form className="form-horizontal" onSubmit={handleSubmit}>
+                <label>
+                    Search weather information by zip code:
+                    <input
+                        type="number"
+                        value={local}
+                        placeholder="Enter zip code"
+                        onChange={handleChange}
+                    />
+                </label>
+                <button className="btn" type=" submit" >
+                    Search
+                </button>
+            </form> */}
+
+
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb" controlId="formBasicEmail"  >
+                    <Form.Label>Search weather information by zip code:</Form.Label>
+                    <Form.Control
+                        type="number"
+                        value={local}
+                        placeholder="Enter zip code"
+                        onChange={handleChange} 
+                        required
+                        size="lg"
+                        />
+
+                </Form.Group>
+
+                <Button className="btn" variant="outline-info" type="submit" size ="lg">
+                    Search
+                </Button>
+            </Form>
+
+
+            <div className="place"> <h1 className="city-nation"> {location.city} {location.nation}</h1> </div>
+
+            {!!location.latitude && !!location.longitude ?
+                (<TheWeather lat={location.latitude} lon={location.longitude} name={location.city}
+                    country={location.country} zip={location.area} />) : null}
+        </div>
+
+
+    )
+
+}
+
+export default WeatherSearch;
+
+
